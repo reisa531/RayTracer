933b94d823871032974cb587e9f881d7759e3206
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-03-25 20:49:13 +0800

    feat: finished phase 2

--- src/main.rs
+++ src/main.rs
@@ -322,8 +322,122 @@ fn cornell_smoke() {
     cam.render(&world);
 }
 
+fn final_scene() {
+    let mut boxes1: HittableList = HittableList::default();
+    let ground = Arc::new(Lambertian::new(0.48, 0.83, 0.53));
+
+    let mut rng = rand::thread_rng();
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + (i as f64) * w;
+            let z0 = -1000.0 + (j as f64) * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_real_interval(&mut rng, 1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.push(Box::new(cuboid(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                ground.clone()
+            )));
+        }
+    }
+
+    let mut world: HittableList = HittableList::default();
+
+    let boxes1_bvh = HittableList::to_bvh(boxes1, &mut rng);
+    world.push(Box::new(boxes1_bvh));
+
+    let light = Arc::new(DiffuseLight::new(7.0, 7.0, 7.0));
+    world.push(Box::new(Quad::new(
+        Point3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        light
+    )));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::new(0.7, 0.3, 0.1));
+    world.push(Box::new(Sphere::new_moving(center1, center2, 50.0, sphere_material)));
+
+    world.push(Box::new(Sphere::new(
+        Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5))
+    )));
+    world.push(Box::new(Sphere::new(
+        Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(0.8, 0.8, 0.9, 1.0))
+    )));
+
+    world.push(Box::new(Sphere::new(
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5))
+    )));
+    let boundary1 = Arc::new(Sphere::new(
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5))
+    ));
+    world.push(Box::new(ConstantMedium::new(boundary1, 0.2, Color::new(0.2, 0.4, 0.9))));
+
+    let boundary2 = Arc::new(Sphere::new(
+        Point3::new(0.0, 0.0, 0.0),
+        5000.0,
+        Arc::new(Dielectric::new(1.5))
+    ));
+    world.push(Box::new(ConstantMedium::new(boundary2, 0.0001, Color::new(1.0, 1.0, 1.0))));
+
+    let emat = Arc::new(Lambertian::from_texture(Arc::new(ImageTexture::new("./assets/earthmap.jpg"))));
+    world.push(Box::new(Sphere::new(Point3::new(400.0, 200.0, 400.0), 100.0, emat)));
+
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.push(Box::new(Sphere::new(
+        Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::from_texture(pertext))
+    )));
+
+    let mut boxes2: HittableList = HittableList::default();
+    let white = Arc::new(Lambertian::new(0.73, 0.73, 0.73));
+    let ns = 1000;
+    for _ in 0..ns {
+        let center = Point3::new(
+            random_real_interval(&mut rng, 0.0, 165.0),
+            random_real_interval(&mut rng, 0.0, 165.0),
+            random_real_interval(&mut rng, 0.0, 165.0)
+        );
+        boxes2.push(Box::new(Sphere::new(center, 10.0, white.clone())));
+    }
+
+    let boxes2_bvh = Arc::new(HittableList::to_bvh(boxes2, &mut rng));
+    world.push(Box::new(Translate::new(
+        Arc::new(RotateY::new(boxes2_bvh, 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0)
+    )));
+
+    let cam = Camera::new(1.0, 800,
+            200, 50, 40.0,
+            Point3::new(478.0, 278.0, -600.0),
+            Point3::new(278.0, 278.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0),
+            0.0, 10.0,
+            Color::new(0.0, 0.0, 0.0));
+
+    let world = HittableList::to_bvh(world, &mut rng);
+
+    cam.render(&world);
+}
+
 fn main() {
-    let scenario = 8;
+    let scenario = 9;
 
     match scenario {
         1 => bouncing_spheres(),
@@ -334,6 +448,7 @@ fn main() {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
+        9 => final_scene(),
         _ => eprintln!("Invalid!")
     }
 }
